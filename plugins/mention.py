966e88b3e81c19c2869bdc7eae66683a8c431dff
me <you@example.com>
2019-04-02 22:24:41 +0900

    bot can become to understand command.

--- plugins/mention.py
+++ plugins/mention.py
@@ -1,18 +1,43 @@
 from slackbot.bot import respond_to
 from slackbot.bot import listen_to
 from slackbot.bot import default_reply
+from datetime import datetime
+import re
 
-#from libs import my_functions
+NOW = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+TOKEN = '|'
 
-@respond_to(r'.+')
-def mention_func(message):
-    print("Hello")
-    text = message.body['text']
-    msg = "オウム返しします" + text
-    message.reply(msg)
+def decode(message):
+    return message.body['text']
+
+@respond_to(r'^[$].*')
+def bot_mention(message):
+    text = decode(message).replace(' ', TOKEN)
+    print(text)
+
+    if re.search(r'[|]', text) is not None:
+        print("arg command")
+        arg_command(message, text)
+    else:
+        none_arg_command(message, text)
 
 
-@listen_to('')
-def listen_func(message):
-    message.send('listen')
-    message.reply('you?')
+def arg_command(message, text):
+    word, arg = text.split(TOKEN)
+
+    if word in "$echo":
+        message.reply(arg)
+
+    else :
+        message.reply("unknown word :" + word)
+
+def none_arg_command(message, text):
+
+    if text in "$timestamp":
+        print("Hello")
+        NOW = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+        message.reply("現在の日時は\n" + NOW)
+
+    else :
+        message.reply("unknown word :" + word)
+
